f8ae91b8910dfe8e723a838e8c56b001bb5989d4
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-05-10 23:42:37 +0700

    feat: respond with authenticated user job

---
+++ app/Jobs/Auth/RespondWithAuthenticatedUser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class RespondWithAuthenticatedUser
+{
+    use Dispatchable;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        /**
+         * The authenticated token was created when attempted.
+         * 
+         * @var \Laravel\Sanctum\NewAccessToken
+         */
+        public \Laravel\Sanctum\NewAccessToken $token
+    ) {
+        
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handle(Request $request): \Illuminate\Http\JsonResponse
+    {
+        return response()->json([
+            'token' => $this->token->plainTextToken,
+            'user' => $request->user()
+        ]);
+    }
+}

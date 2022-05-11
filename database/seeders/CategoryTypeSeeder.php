9d994599691c8f3f5a97bf657a33086dcdefa302
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-05-11 21:41:02 +0700

    feat: category type seeder

---
+++ database/seeders/CategoryTypeSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CategoryType;
+use Illuminate\Database\Seeder;
+
+class CategoryTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        CategoryType::insert(CategoryType::CATEGORY_TYPES);
+    }
+}
